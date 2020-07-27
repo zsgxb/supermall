0280be0ac7b6735e0be82184f5ef8ea858857142
s <849815727@qq.com>
2020-07-27 18:26:37 +0800

    风格配置和别名配置

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports={
+	configgureWebpack:{
+		resolve:{
+			alias:{
+				'assets':'@assets',
+				'common':'@common',
+				'components':'@components',
+				'network':'@network',
+				'views':'@views',
+			}
+		}
+	}
+}
