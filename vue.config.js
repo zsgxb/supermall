f044407b56f7e044b57c17ba75c45ade6bc360b3
gxm <849815727@qq.com>
2020-07-30 01:13:15 +0800

    模块

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,14 @@
-module.exports={
-	configgureWebpack:{
-		resolve:{
-			alias:{
-				'assets':'@assets',
-				'common':'@common',
-				'components':'@components',
-				'network':'@network',
-				'views':'@views',
-			}
-		}
-	}
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views',
+      }
+    }
+  }
 }
+
