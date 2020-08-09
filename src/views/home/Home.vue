7166260c2a727c42cb88436b2295915dc008d1dd
gxm <849815727@qq.com>
2020-08-09 13:50:42 +0800

    首页

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,30 +1,43 @@
 <template>
   <div>
     <!-- <h2>首页</h2> -->
-      <nav-bar class="home-nav">
-        <div  slot="center">购物街</div>
-      </nav-bar>
+      <nav-bar class="home-nav"><div  slot="center">购物街</div></nav-bar>
+      <home-swiper :banners="banners" />
+      <recommend-view :recommend="recommends"/>
   </div>
 </template>
 
 <script>
   import NavBar from  'components/common/navbar/NavBar'
-  import {getHomemultidata} from 'network/home.js'
+  import HomeSwiper from './childComps/HomeSwiper.vue'
+  import RecommendView from './childComps/RecommendView.vue'
+
+   import {getHomemultidata} from 'network/home.js'
 
   export default {
     name: "Home",
     components:{
-      NavBar
+      NavBar,
+      HomeSwiper,
+      RecommendView
+    },
+    data(){
+      return {
+        banners:[],
+        recommends:[]
+      }
     },
     created(){
          getHomemultidata().then(res=>{
-            console.log(res)
+           console.log(res)
+            this.banners  =res.data.banner.list,
+            this.recommends=res.data.recommend.list
           })
     }
   }
 </script>
 
-<style scoped> 
+<style scoped>
   .home-nav{
     background: lightpink;
     color: white;
